22056cf341c733026864384c0b45dda868227b4c
Lee_gw <lgs2940aa@gmail.com>
2025-02-17 19:18:18 +0900

    character

--- 2nd-Team1-CH3-Project/Source/CH3_TeamProject/Private/MyGameMode.cpp
+++
@@ -1,9 +0,0 @@
-#include "MyGameMode.h"
-#include "MyCharacter.h"
-#include "MyPlayerController.h"
-
-AMyGameMode::AMyGameMode()
-{
-	DefaultPawnClass = AMyGameMode::StaticClass();
-	PlayerControllerClass = AMyPlayerController::StaticClass();
-}
